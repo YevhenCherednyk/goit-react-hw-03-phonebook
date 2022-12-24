e29f9baf54ea0ac6d093e6b6b8e6842ab2aa4ecb
Yevhen Cherednyk <yevgen.cherednyk@gmail.com>
2022-12-24 21:49:17 +0200

    finish task

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,16 +6,28 @@ import ContactList from 'components/ContactList';
 import Filter from 'components/Filter';
 import Section from 'components/Section';
 
+const STORAGE_KEY = 'contacts';
+
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem(STORAGE_KEY);
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = (name, number) => {
     const newContact = { id: nanoid(), name, number };
